3d03ac1dbcd3470622bf97f67930b73ef01856cb
millcour <courtney.miller005@gmail.com>
2018-04-22 12:22:27 -0700

    returning puzzle

--- lab1/server.py
+++ lab1/server.py
@@ -60,7 +60,10 @@ def hide_bits(preImage, k):
 
 # generate the puzzle for the client to solve
 def generate_puzzle():
-    return random_string(4)
+    compute = str(get_time()) + random_string() + random_string()
+    hash_string(compute)
+    pre_image = hide_bits(1)
+    return hash_string(pre_image)
 
 # next create a socket object
 s = socket.socket()
