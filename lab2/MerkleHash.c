6fcc213faea3dc4dc47d07517767411180b9e89e
hayden <anderrob@oregonstate.edu>
2018-05-14 02:05:43 -0700

    back to almost working

--- lab2/MerkleHash.c
+++ lab2/MerkleHash.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/sha.h>
-
+#include <limits.h>
 #define COUNT 10
 #define TREE_HEIGHT 3;
 
@@ -18,32 +18,44 @@ typedef struct node
     struct node *parent;
 }node;
 
+// typedef struct stack
+// {
+//     char* data;
+//     struct stack* next;
+// }stack;
+ 
 struct stack
 {
-    char* data;
-    struct stack* next;
+    int top;
+    unsigned capacity;
+    char** array;
 };
  
-
  
 char global_hash[SHA_DIGEST_LENGTH*2] = {0};
-
+char temp[1024];
 struct node* new_node(char* data);
-int isEmpty(struct stack *stack_head);
-void push(struct stack** stack_head, char* data);
-char* pop(struct stack** stack_head);
-int peek(struct stack* stack_head);
+// int isEmpty(struct stack *stack_head);
+// void push(struct stack** stack_head, char* data);
+// char* pop(struct stack** stack_head);
+// int peek(struct stack* stack_head);
 void print2DUtil(node *root, int space);
 void print2D(node *root);
 char* hash(char* data);
 char* get_root_hash(node *root);
-void authentication_values(node*root, node *leaf, struct stack*** stack_head);
-void pop_stack(struct stack** stack_head);
-struct stack* newNode(char* data);
-int siblings_hash[2];
+
+void pop_stack(struct stack* stack_head);
+// struct stack* newNode(char* data);
+char* siblings_hash;
 
 
+char* pop(struct stack* stack);
+void push(struct stack* stack, int item);
+int isEmpty(struct stack* stack);
+int isFull(struct stack* stack);
+struct stack* createStack(unsigned capacity);
 
+void authentication_values(node* root, node *leaf, struct stack* stack);
 
 
 int main(){
@@ -53,7 +65,7 @@ int main(){
     root->left->parent = root;
     root->right  = new_node("node 3");
     root->right->parent = root;
-    struct stack* stack = NULL;
+    struct stack* stack = createStack(5);
 
 
     root->left->left  = new_node("node 4");
@@ -79,7 +91,7 @@ int main(){
     
 
     printf("leaf is %s\nAuthentication hashes are:\n", root->right->left->hash);
-    authentication_values(root, root->right->left, &stack);
+    authentication_values(root, root->right->left, stack);
     printf("Hashes needed to verify leaf %s are:\n", root->right->left->data);
     pop_stack(stack);
  
@@ -87,20 +99,22 @@ int main(){
 }
 
 
-void authentication_values(node* root, node *leaf, struct stack*** stack_head){
+void authentication_values(node* root, node *leaf, struct stack* stack){
 
     if(leaf->hash == root->hash){
         return;
     }
     if(leaf->parent->left->hash == leaf->hash){
         //printf("\t\t%d\n",leaf->parent->right->hash);
-        push(&*stack_head, leaf->parent->right->hash);
+        //push(&*stack_head, leaf->parent->right->hash);
+        push(stack, leaf->parent->right->hash);
     }
     else if (leaf->parent->right->hash == leaf->hash){
         //printf("\t\t%d\n",leaf->parent->left->hash);
-        push(&*stack_head, leaf->parent->left->hash);
+        //push(&*stack_head, leaf->parent->left->hash);
+        push(stack, leaf->parent->left->hash);
     }
-    authentication_values(root, leaf->parent, stack_head);
+    authentication_values(root, leaf->parent, stack);
 
 }
 
@@ -159,17 +173,21 @@ void hash_tree(node *root)
 
 
 char* hash(char* data){
-    // char temp[1024];
-    // strcpy(temp, data);
-    // return strcat(temp, "hashed" );
-    return data;
+    
+    strcpy(global_hash, data);
+    //printf("hash temp: %s\n", global_hash);
+    strcat(global_hash, "hashed" );
+    //printf("hash2 temp: %s\n", temp);
+    return global_hash;
+    
+    //return data;
 }
 
 
-void pop_stack(struct stack** stack_head){
+void pop_stack(struct stack* stack_head){
     
-    while (peek(stack_head) > 0){
-        printf("\t\t%s\n", pop(&stack_head));
+    while (stack_head->top != -1  ){
+        printf("\t\t\t%s\n", pop(stack_head));
     }
     
 }
@@ -213,47 +231,84 @@ void print2D(node *root)
 
 // Source: https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
 
-int isEmpty(struct stack *stack_head)
+// function to create a stack of given capacity. It initializes size of
+// stack as 0
+struct stack* createStack(unsigned capacity)
 {
-    return !stack_head;
+    struct stack* stack = (struct stack*) malloc(sizeof(struct stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (char**) malloc(stack->capacity * sizeof(int));
+    return stack;
 }
  
-void push(struct stack** stack_head, char* data)
+// Stack is full when top is equal to the last index
+int isFull(struct stack* stack)
+{   return stack->top == stack->capacity - 1; }
+ 
+// Stack is empty when top is equal to -1
+int isEmpty(struct stack* stack)
+{   return stack->top == INT_MIN;  }
+ 
+// Function to add an item to stack.  It increases top by 1
+void push(struct stack* stack, int item)
 {
-    struct stack* stack_node = newNode(data);
-    stack_node->next = *stack_head;
-    *stack_head = stack_node;
-    printf("\t\t%s\n", data);
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = item;
+    printf("\t\t\t%s\n", item);
 }
  
-char* pop(struct stack** stack_head)
+// Function to remove an item from stack.  It decreases top by 1
+char* pop(struct stack* stack)
 {
-    if (isEmpty(*stack_head))
-        return NULL;
-    struct stack* temp = *stack_head;
-    *stack_head = (*stack_head)->next;
-    char popped[1024];
-    printf("in pop, temp->data: %s\n", temp->data);
-    strcpy(popped, temp->data);
-    printf("in pop after strcpy, popped: %s\n", popped);
-
-    free(temp);
-    printf("in pop after strcpy, popped after free temp: %s\n", popped);
-    return popped;
-}
- 
-int peek(struct stack* stack_head)
-{
-    if (isEmpty(stack_head))
+    if (isEmpty(stack))
         return -1;
-    return stack_head->data;
+    return stack->array[stack->top--];
 }
 
-struct stack* newNode(char* data)
-{
-    struct stack* stack_node =
-              (struct stack*) malloc(sizeof(struct stack));
-    stack_node->data = data;
-    stack_node->next = NULL;
-    return stack_node;
-}
+
+// int isEmpty(struct stack *stack_head)
+// {
+//     return !stack_head;
+// }
+ 
+// void push(struct stack** stack_head, char* data)
+// {
+//     struct stack* stack_node = newNode(data);
+//     stack_node->next = *stack_head;
+//     *stack_head = stack_node;
+//     printf("\t\t%s\n", data);
+// }
+ 
+// char* pop(struct stack** stack_head)
+// {
+//     if (isEmpty(*stack_head))
+//         return NULL;
+//     struct stack* temp = *stack_head;
+//     *stack_head = (*stack_head)->next;
+//     char popped[1024];
+//     printf("in pop, temp->data: %s\n", temp->data);
+//     strcpy(popped, temp->data);
+//     printf("in pop after strcpy, popped: %s\n", popped);
+
+//     free(temp);
+//     printf("in pop after strcpy, popped after free temp: %s\n", popped);
+//     return popped;
+// }
+ 
+// int peek(struct stack* stack_head)
+// {
+//     if (isEmpty(stack_head))
+//         return -1;
+//     return stack_head->data;
+// }
+
+// struct stack* newNode(char* data)
+// {
+//     struct stack* stack_node =
+//               (struct stack*) malloc(sizeof(struct stack));
+//     stack_node->data = data;
+//     stack_node->next = NULL;
+//     return stack_node;
+// }
