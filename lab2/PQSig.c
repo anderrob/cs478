6bb2d5706cae883d4d7e4c52c31003dd97f78f0a
millcour <courtney.miller005@gmail.com>
2018-05-16 21:00:22 -0700

    added mostly complete function for signing

--- lab2/PQSig.c
+++ lab2/PQSig.c
@@ -11,7 +11,9 @@ typedef struct {
 } pk;
 
 char* itob(int i);
-void key(int l, int k, int t);
+void pq_key_gen(int l, int k, int t, sk secret, pk public);
+void pq_sign (int k, int t, char* m, sk secret);
+int h[33] = {0};
 
 sk secret;
 pk public;
@@ -19,18 +21,24 @@ char* concat[1153] = {'\0'};
 char s[1024][SHA_DIGEST_LENGTH*2+1] = {'\0'};
 char v[1024][SHA_DIGEST_LENGTH*2+1] = {'\0'};
 
+char sign_s[1024][SHA_DIGEST_LENGTH*2+1] = {'\0'};
+char sign_v[1024][SHA_DIGEST_LENGTH*2+1] = {'\0'};
+
 int main() {
   int t = 1024;
   int k = 32;
   int l = 128;
-  key(l, k, t);
+  sk secret;
+  pk public;
+  pq_key_gen(l, k, t, secret, public);
+  pq_sign(k, t, "hello", secret);
 
   //TODO: Form the merkle tree using v
 
   return 0;
 }
 
-void key(int l, int k, int t) {
+void pq_key_gen(int l, int k, int t, sk secret, pk public) {
   public.d = 1;
   secret.st = 1;
   secret.z = random_string(128);
@@ -42,6 +50,35 @@ void key(int l, int k, int t) {
     strcpy(s[i], hash);
     hash_string(s[i]);
     strcpy(v[i], hash);
+  }
+}
+
+void pq_sign (int k, int t, char* m, sk secret) {
+  char* binary;
+  int size = log2(t);
+  if(secret.st > public.d){
+    exit(0);
+  } else {
+    hash_string(m);
+    binary = string_to_binary(hash);
+    divide_string(binary, size);
+    for(int i=0; i<k; i++){
+      h[i] = btoi(split[i]);
+    }
+    for(int j=0; j<k; j++){
+      int temp = (secret.st-1)+h[j];
+
+      memset(concat, '\0', sizeof(concat));
+      strcpy(concat, secret.z);
+      strcat(concat, itob(temp));
+      hash_string(concat);
+      strcpy(sign_s[temp], hash);
+      hash_string(sign_s[temp]);
+      strcpy(sign_v[temp], hash);
+      // get path for leaf
+      // assign things to signature
+      secret.st++;
+    }
   }
 }
 
